e5897da68c2e5824806d8ff8613904a1fff44aa0
Ricardo Buquet <ricardo.buquet@groupon.com>
2015-01-20 15:27:49 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jslindo::Application.config.secret_token = '14f42182b5f5cdf4492377461436b01c23a097dbb92af32128e6dc9c85355d1a669f053dea7833c33d5cd6802c0bb52a79f74e602f84fcc0514cc5318caa300f'
